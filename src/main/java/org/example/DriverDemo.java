4d4c841cd06386e6b82ea3d2441b57db87ff2512
Kunal <kunals7888@gmail.com>
2025-09-30 10:11:27 +0530

    XPath and CSS selectors

--- src/main/java/org/example/DriverDemo.java
+++ src/main/java/org/example/DriverDemo.java
@@ -24,6 +24,22 @@ public class DriverDemo {
         // Using different types of Locators in Selenium Web Driver
 
         WebElement usernameInputField = driver.findElement(By.id("username"));
+
+        // Using xPath and css selector
+        WebElement usernameInputFieldXpath = driver.findElement(By.xpath("//input[@id='username']"));
+        WebElement usernameInputFieldCss = driver.findElement(By.cssSelector("input[id='username']"));
+
+
+        /*
+         For manual verification of Xpath and CSS selector
+         Run the following commands on console in inspect window
+
+         For xPath --> $x('//*[@id="username"]')
+
+         For css --> $$('#username')
+
+         */
+
         WebElement passwordInputField = driver.findElement(By.name("password"));
         WebElement submitBtn = driver.findElement(By.className("btn"));
 
@@ -31,14 +47,16 @@ public class DriverDemo {
         List<WebElement> inputFields = driver.findElements(By.tagName("input"));
 
         // Link and Partial Link Text
-        WebElement footerLink = driver.findElement(By.linkText("Practice Test Automation"));
+        WebElement footerLink = driver.findElement(By.linkText("Practice Test Automation."));
         WebElement footerPartialLink = driver.findElement(By.partialLinkText("Test Automation"));
 
         // Locating elements using RelativeLocator
         WebElement relativePasswordLocate = driver.findElement(RelativeLocator.with(By.tagName("input")).below(By.id("username")));
-        WebElement relativeFooterLinkLocate = driver.findElement(RelativeLocator.with(By.tagName("a")).toRightOf(By.linkText("Practice Test Automation")));
+        WebElement relativeFooterLinkLocate = driver.findElement(RelativeLocator.with(By.tagName("a")).toRightOf(By.linkText("Practice Test Automation.")));
 
-
+        // For comppund class names example for home button look for unique substring in classname
+        // Example : class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home menu-item-43" ---> "menu-item-home"
+        WebElement homeButtonSelector = driver.findElement(By.className("menu-item-home"));
 
     }
 
